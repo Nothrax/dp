d5e53dd001e7d7125c8032c3bbbf69f0f5c43bb3
Jakub Trubka <trubka.jakub@gmail.com>
2023-11-17 14:34:50 +0100

    Refactor wine cellar device code

--- device/wine_cellar/device/WineCellarDevice.ino
+++ device/wine_cellar/device/WineCellarDevice.ino
@@ -3,114 +3,154 @@
 #include <VirtualWire.h>
 #include "MHZ19.h"
 #include "DHT.h"
-//https://navody.arduino-shop.cz/navody-k-produktum/detektor-oxidu-uhliciteho-mh-z19b.html
 
-// Set the LCD address to 0x3F for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-struct Message{
-    uint32_t counter;
-    float temperature;
-    float humidity;
-    uint32_t carbonDioxide;
-    uint32_t checkSum;
-};
+/// Humidity and temperature sensor DHT22 defines
+/// Pin number of arduino where the DATA pin of DHT sensor
+#define DHT_PIN 2
+// Sensor is of type DHT22
+#define DHT_TYPE DHT22
+/// Instance of the DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
 
-//vlhkost a teplota DHT22
-#define DHTPIN 2                                 //číslo pinu Arduina, kam je připojený DATA pin senzoru DHT
-#define DHTTYPE DHT22                            //senzor je typp DHT22
-DHT dht(DHTPIN, DHTTYPE);                        // do proměné dht uložíme údaje o již nadefinovaném senzoru
-
-// CO2 a teplota mhz19
+/// CO2 and temperature sensor MHZ19 defines
+/// PWM pin of MHZ19
 #define MHZ19_PWN_PIN 9
-#define MHZ19_RX_PIN 11                          //nastavení propojovacích pinů CO2 senzor !pozor tx s rx a rx s tx
+/// RX pin of MHZ19
+#define MHZ19_RX_PIN 11
+/// TX pin of MHZ19
 #define MHZ19_TX_PIN 10
-MHZ19 *mhz19_uart = new MHZ19(MHZ19_RX_PIN, MHZ19_TX_PIN);   // vytvoření objektů z knihovny CO2 senzor mhz19
+/// 433MHz communication chip defines
+
+/// RF board (NiceRF 433MHz wl101-341) specific settings
+#define RF_WIRELESS_PIN 7
+#define RF_BITS_PER_SECOND 512
+
+/// Pin of indication led
+#define LED_PIN 8
+/// Data rate on serial port
+#define SERIAL_DATA_RATE 9600
+
+/// CO2 threshold, after reaching this value, red led will be turned on
+#define CO2_THRESHOLD 4900;
+/// How often will the values be measured and sended using RF board
+#define MEASUREMENT_DELAY_MS 15000
+
+/// Instances of MHZ19 sensor
+MHZ19 *mhz19_uart = new MHZ19(MHZ19_RX_PIN, MHZ19_TX_PIN);
 MHZ19 *mhz19_pwm = new MHZ19(MHZ19_PWN_PIN);
 
-//433MHz komunikace
-#define WIRELESS_PIN 7
-#define LED_PIN 8
-int messageCounter = 0;
+// Set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3f, 16, 2);
 
-void sendData(uint8_t *data, int dataSize);
-uint32_t calculateCheckSum(uint8_t *data, int dataSize);
+/**
+ * Definition of the RF message
+**/
+struct RFMessage {
+	/// Message counter
+	uint32_t counter;
+	/// Current temperature
+	float temperature;
+	/// Current humidity
+	float humidity;
+	/// Currenc CO2 level
+	uint32_t carbonDioxide;
+	/// Check sum
+	uint32_t checkSum;
+};
+
+/// Message counter of RF messages
+int rfMessageCounter { 0 };
+
+/**
+ * Send data using 433Mhz chip
+**/
+void sendData(const uint8_t *data, int dataSize);
+
+/**
+ * Calculate 32bit checksum from given values
+**/
+uint32_t calculateCheckSum(const uint8_t *data, int dataSize);
 
 void setup() {
-    lcd.init();                                   //nastaveni lcd
-    lcd.backlight();
-    lcd.setCursor(0,0);
+	/// LCD screen init
+	lcd.init();
+	lcd.backlight();
+	lcd.setCursor(0, 0);
 
-    //todo led led led
-    pinMode(LED_PIN, OUTPUT);
+	/// Set led pin as output
+	pinMode(LED_PIN, OUTPUT);
 
-    Serial.begin(9600);                           // zahájení komunikace po sériové lince
+	// Serial communication
+	Serial.begin(SERIAL_DATA_RATE);
 
-    mhz19_uart->begin(MHZ19_RX_PIN, MHZ19_TX_PIN);// zahájení komunikace se senzorem mhz19 přes UART
-    mhz19_uart->setAutoCalibration(false);        // vypnutí autokalibrace
+	/// Initialization of communication with MHZ19
+	mhz19_uart->begin(MHZ19_RX_PIN, MHZ19_TX_PIN);
+	mhz19_uart->setAutoCalibration(false);
 
-    dht.begin();                                  // aktivujeme komunikaci senzoru DHT
 
-    // nastavení typu bezdrátové komunikace
-    vw_set_ptt_inverted(true);
-    // nastavení rychlosti přenosu v bitech za sekundu
-    vw_setup(512);
-    // nastavení čísla datového pinu pro vysílač
-    vw_set_tx_pin(WIRELESS_PIN);
-    digitalWrite(LED_PIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_PIN, LOW); 
-    delay(1000);
-    digitalWrite(LED_PIN, HIGH);
-    delay(1000);
+	/// Initialization of communication with DHT
+	dht.begin();
+
+	/// 433Mhz chip setup
+	vw_set_ptt_inverted(true);
+	vw_setup(RF_BITS_PER_SECOND);
+	vw_set_tx_pin(WIRELESS_PIN);
+
+	/// Init done indication using leds
+	digitalWrite(LED_PIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_PIN, LOW);
+	delay(1000);
+	digitalWrite(LED_PIN, HIGH);
+	delay(1000);
 }
 
 void loop() {
-    //MHZ19
-    measurement_t m = mhz19_uart->getMeasurement();
-    int co2 = (mhz19_pwm->getPpmPwm() + map(m.co2_ppm, 0, 5000, 0, 2000))/2;
+	//MHZ19 reading
+	measurement_t m = mhz19_uart->getMeasurement();
+	int co2 = (mhz19_pwm->getPpmPwm() + map(m.co2_ppm, 0, 5000, 0, 2000))/2;
 
-    //DHT22
-    //todo isnan(hum) || isnan(temp)
-    float hum = dht.readHumidity();
-    float temp = dht.readTemperature();
+	/// DHT22 reading
+	//todo isnan(hum) || isnan(temp)
+	float hum = dht.readHumidity();
+	float temp = dht.readTemperature();
 
-    if(co2 > 4900){  
-      digitalWrite(LED_PIN, HIGH); 
-    }else{
-      digitalWrite(LED_PIN, LOW); 
-    }
+	/// If CO2 is over threshold, turn on warning led
+	if(co2 > co2Threshold) {
+		digitalWrite(LED_PIN, HIGH);
+	} else {
+		digitalWrite(LED_PIN, LOW);
+	}
 
-    //zapis na lcd
-    lcd.setCursor(0,0);
-    lcd.clear();
-    lcd.print(co2);
-    lcd.print("PPM ");
-    //lcd.print(m.temperature); //teplota z MHZ19
-    lcd.print(temp);
-    lcd.print("\xDF" "C");
+	/// Lcd write of measured values
+	lcd.setCursor(0, 0);
+	lcd.clear();
+	lcd.print(co2);
+	lcd.print("PPM ");
+	lcd.print(temp);
+	lcd.print("\xDF" "C");
 
-    lcd.setCursor(0,1);
-    lcd.print(hum);
-    lcd.print("%");
+	lcd.setCursor(0, 1);
+	lcd.print(hum);
+	lcd.print("%");
 
-    //todo cervena ledka
 
-    Message message = {messageCounter,temp,hum,co2, 0};
-    messageCounter++;
-    message.checkSum = calculateCheckSum((uint8_t *)&message, 20);
-    sendData((uint8_t *)&message, sizeof(Message));
-    delay(15000);
+	Message message = { messageCounter, temp, hum, co2, 0 };
+	messageCounter++;
+	message.checkSum = calculateCheckSum((uint8_t*) & message, 20);
+	sendData((uint8_t*) & message, sizeof(Message));
+	delay(MEASUREMENT_DELAY_MS);
 }
 
-void sendData(uint8_t *data, int dataSize){
-    vw_send((uint8_t *)data, dataSize);
-    vw_wait_tx();
+void sendData(const uint8_t *data, int dataSize) {
+	vw_send((uint8_t *)data, dataSize);
+	vw_wait_tx();
 }
 
-uint32_t calculateCheckSum(uint8_t *data, int dataSize){
-    uint32_t checkSum = 0;
-    for(int i = 0; i < dataSize; i++){
-        checkSum += data[i];
-    }
-    return checkSum;
+uint32_t calculateCheckSum(const uint8_t *data, int dataSize) {
+	uint32_t checkSum = 0;
+	for(int i = 0; i < dataSize; i++) {
+		checkSum += data[i];
+	}
+	return checkSum;
 }
